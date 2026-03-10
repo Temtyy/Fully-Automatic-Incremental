d594bf17fb973ab6cfc0c84cd823277e581ff242
Temtyy <113525868+Temtyy@users.noreply.github.com>
2026-03-10 21:29:16 +0100

    Reset player points to zero

--- js/save.js
+++ js/save.js
@@ -1,5 +1,5 @@
 let player = {
-    points: new Decimal("1e165"),
+    points: new Decimal("0"),
     superPoints: new Decimal(0),
     superPointsSubtracted: new Decimal(0), //worst way to do this but i'm making it like this anyway
     upgrades: []
